8d544098b6fd2d602bb5d4636b8b321247e86699
Toni Rajkovski <toni.rajkovski@gmail.com>
2016-11-28 00:35:06 +0100

    removed unneeded dependency

--- app/src/main/java/com/rajkovski/toni/transportdemo/MainActivity.java
+++ app/src/main/java/com/rajkovski/toni/transportdemo/MainActivity.java
@@ -1,22 +1,17 @@
 package com.rajkovski.toni.transportdemo;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.caverock.androidsvg.SVG;
-import com.caverock.androidsvg.SVGParseException;
 import com.rajkovski.toni.transportdemo.logger.Logger;
 import com.rajkovski.toni.transportdemo.model.Schema_template;
 import com.rajkovski.toni.transportdemo.services.MainService;
 import com.rajkovski.toni.transportdemo.services.svg.SvgService;
 import com.scand.svg.SVGHelper;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import javax.inject.Inject;
