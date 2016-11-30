f18f117bc43a784ad98bf5664ebe7d4fb31656f8
Toni Rajkovski <toni.rajkovski@gmail.com>
2016-11-30 19:13:17 +0100

    implemented displaying of the polylines

--- app/src/main/java/com/rajkovski/toni/transportdemo/ui/map/MapActivity.java
+++ app/src/main/java/com/rajkovski/toni/transportdemo/ui/map/MapActivity.java
@@ -1,50 +1,77 @@
 package com.rajkovski.toni.transportdemo.ui.map;
 
-import android.support.v4.app.FragmentActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 import com.rajkovski.toni.transportdemo.R;
+import com.rajkovski.toni.transportdemo.model.Route;
+import com.rajkovski.toni.transportdemo.model.Segment;
+
+import java.util.List;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
   public static final String INTENT_KEY_ROUTE_FOR_MAP = "transport_demo.route_for_map";
 
-  private GoogleMap mMap;
+  private GoogleMap map;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.map_activity);
-    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
       .findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
   }
 
-  /**
-   * Manipulates the map once available.
-   * This callback is triggered when the map is ready to be used.
-   * This is where we can add markers or lines, add listeners or move the camera. In this case,
-   * we just add a marker near Sydney, Australia.
-   * If Google Play services is not installed on the device, the user will be prompted to install
-   * it inside the SupportMapFragment. This method will only be triggered once the user has
-   * installed Google Play services and returned to the app.
-   */
   @Override
   public void onMapReady(GoogleMap googleMap) {
-    mMap = googleMap;
+    map = googleMap;
 
-    //TODO do something with the route
-
-    // Add a marker in Sydney and move the camera
-    LatLng sydney = new LatLng(-34, 151);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    Route route = (Route) getIntent().getSerializableExtra(INTENT_KEY_ROUTE_FOR_MAP);
+    addRouteToMap(route);
   }
+
+  private void addRouteToMap(Route route) {
+    final LatLngBounds.Builder boundsBuilder = LatLngBounds.builder();
+    if (route != null && route.getSegments().size() > 0) {
+      for (Segment segment : route.getSegments()) {
+        if (segment.getPolyline() != null) {
+          PolylineOptions polylineOptions = new PolylineOptions();
+          List<LatLng> polylineLatLng = PolyUtil.decode(segment.getPolyline());
+          for (LatLng latLng : polylineLatLng) {
+            polylineOptions.add(latLng);
+            boundsBuilder.include(latLng);
+          }
+          polylineOptions.color(Color.parseColor(segment.getColor()));
+          map.addPolyline(polylineOptions);
+          map.setOnPolylineClickListener(new GoogleMap.OnPolylineClickListener() {
+            @Override
+            public void onPolylineClick(Polyline polyline) {
+              // TODO display segment details
+            }
+          });
+        }
+      }
+      map.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+         @Override
+         public void onMapLoaded() {
+           map.animateCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 300));
+         }
+       });
+    }
+  }
+
+
 }
